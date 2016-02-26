34dcc8eb38c0a2d00391d50cd97d36b4a16aa17b
Ning Zhiming <zmning@acorn-net.com>
2016-02-26 20:02:55 +0800

    finish time calc

--- test.c
+++ test.c
@@ -1,40 +1,5 @@
-/* #include <libunwind.h> */
 #include <stdio.h>
 #include <time.h>
-#if 0
-void do_unwind_backtrace()
-{
-    unw_cursor_t    cursor;
-    unw_context_t   context;
-
-    unw_getcontext(&context);
-    unw_init_local(&cursor, &context);
-
-    while (unw_step(&cursor) > 0) {
-        unw_word_t  offset, pc;
-        char        fname[64];
-
-        unw_get_reg(&cursor, UNW_REG_IP, &pc);
-
-        fname[0] = '\0';
-        (void) unw_get_proc_name(&cursor, fname, sizeof(fname), &offset);
-
-        printf ("%p : (%s+0x%x) [%p]\n", pc, fname, offset, pc);
-    }
-    printf("---------------------------------------------------------\n");
-}
-
-void bar()
-{
-    do_unwind_backtrace();
-    return;
-}
-void foo()
-{
-    bar();
-    return;
-}
-#endif
 
 typedef struct time_val_base {
     long yeartime;
@@ -84,12 +49,13 @@ static inline int isleapyear(int year)
     return  (((year % 4) ==0) && (((year % 100) != 0) || ((year % 400) == 0)));
 }
 
-int calc_gmt_tm(time_t input_time, struct tm* out_tm)
+void calc_gmt_tm(time_t input_time, struct tm* out_tm)
 {
     time_t time_source = input_time;
     time_t least;
     struct tm time_val;
     int tmp = 0;
+    int step, m_step;
 
     if (time_source == 0) {
         time_source = time(NULL);
@@ -97,7 +63,7 @@ int calc_gmt_tm(time_t input_time, struct tm* out_tm)
     tmp = time_source/leapyear_round_time_cost;
     least = time_source - tmp * leapyear_round_time_cost;
     tmp *= 4;
-    for (int step = 0; step < 4; step++) {
+    for (step = 0; step < 4; step++) {
         if (least > leapyear_step[step]) {
             tmp += 1;
             least -= leapyear_step[step];
@@ -105,9 +71,10 @@ int calc_gmt_tm(time_t input_time, struct tm* out_tm)
             break;
         }
     }
-    time_val.tm_year = tmp + 1970;
+    time_val.tm_year = tmp + 70;
 
-    for (int m_step = 0; m_step < 12; m_step++) {
+    time_val.tm_mon = 0;
+    for (m_step = 0; m_step < 12; m_step++) {
         int month_sec = g_timebase.monthtime[m_step];
         if(m_step == 1 && isleapyear(time_val.tm_year)) {
             month_sec += g_timebase.daytime;
@@ -115,7 +82,7 @@ int calc_gmt_tm(time_t input_time, struct tm* out_tm)
         if (least > month_sec) {
             least -= month_sec;
         } else {
-            time_val.tm_mon = m_step + 1;
+            time_val.tm_mon = m_step;
             break;
         }
     }
@@ -132,8 +99,8 @@ int calc_gmt_tm(time_t input_time, struct tm* out_tm)
 
     time_val.tm_sec = least;
 
-    printf("%d %d %d %d %d %d\n", time_val.tm_year, time_val.tm_mon,
-    		time_val.tm_mday, time_val.tm_hour, time_val.tm_min, time_val.tm_sec);
+    /* printf("%d %d %d %d %d %d\n", time_val.tm_year, time_val.tm_mon, */
+            /* time_val.tm_mday, time_val.tm_hour, time_val.tm_min, time_val.tm_sec); */
     if (out_tm) {
         out_tm->tm_year = time_val.tm_year;
         out_tm->tm_mon  = time_val.tm_mon;
@@ -141,38 +108,76 @@ int calc_gmt_tm(time_t input_time, struct tm* out_tm)
         out_tm->tm_hour = time_val.tm_hour;
         out_tm->tm_min  = time_val.tm_min;
         out_tm->tm_sec  = time_val.tm_sec;
-
     }
-    return 0;
 }
 
-struct tm* calc_china_tm(time_t input_time, struct tm* out_tm)
+void calc_timezone_tm(time_t input_time, struct tm* out_tm, int timezone)
 {
-#define CHINA_TIME_ZONE  8
     int month_last_day;
     calc_gmt_tm(input_time, out_tm);
 
-    month_last_day = g_timebase.month_day[out_tm->tm_mon] + isleapyear(out_tm->tm_year);
     if(out_tm) {
-        out_tm->tm_hour += CHINA_TIME_ZONE;
+        out_tm->tm_hour += timezone;
         if (out_tm->tm_hour > 24) {
             out_tm->tm_hour -= 24;
             out_tm->tm_mday += 1;
+            month_last_day = g_timebase.month_day[out_tm->tm_mon] + isleapyear(out_tm->tm_year);
             if (out_tm->tm_mday > month_last_day) {
                 out_tm->tm_mday = 1;
                 out_tm->tm_mon += 1;
-                if (out_tm->tm_mon > 12) {
-                    out_tm->tm_mon = 1;
+                if (out_tm->tm_mon == 12) {
+                    out_tm->tm_mon = 0;
                     out_tm->tm_year += 1;
                 }
             }
+        } else if (out_tm->tm_hour < 0) {
+            out_tm->tm_hour += 24;
+            out_tm->tm_mday -= 1;
+            if (out_tm->tm_mday < 1) {
+                out_tm->tm_mon -= 1;
+                if (out_tm->tm_mon < 0 ) {
+                    out_tm->tm_mon = 11;
+                    out_tm->tm_year -= 1;
+                }
+                month_last_day = g_timebase.month_day[out_tm->tm_mon] + isleapyear(out_tm->tm_year);
+                out_tm->tm_mday = month_last_day;
+            }
         }
     }
-    return out_tm;
 }
 
+void calc_china_tm(time_t input_time, struct tm* out_tm)
+{
+#define CHINA_TIME_ZONE 8
+    calc_timezone_tm(input_time, out_tm, CHINA_TIME_ZONE);
+}
+
+char* get_local_time(time_t timesrc, char* outbuf, size_t outbuflen)
+{
+    static char buffer[128];
+    char* time_buf = outbuf == NULL ? buffer : outbuf;
+    size_t len = outbuf == NULL ? sizeof(buffer) : outbuflen;
+    time_t now = timesrc;
+    struct tm localtm = {0};
+    if (timesrc == 0) {
+        now = time(NULL);
+    }
+
+    calc_china_tm(now, &localtm);
+    snprintf(time_buf, len, "%d-%02d-%02d %02d:%02d:%02d",
+            localtm.tm_year + 1900,
+            localtm.tm_mon + 1,
+            localtm.tm_mday,
+            localtm.tm_hour,
+            localtm.tm_min,
+            localtm.tm_sec
+            );
+    time_buf[len - 1] = '\0';
+    return time_buf;
+
+}
 int main(int argc, const char *argv[])
 {
-	calc_gmt_tm(0, NULL);
+    printf("%s\n", get_local_time(0, NULL, 0));
     return 0;
 }
